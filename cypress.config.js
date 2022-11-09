6a2ccad998b7dd14af78e7fecd72897cd7dc802f
arianemadureira <arianemadureira@live.com>
2022-11-09 14:13:55 -0300

    Add projeto inicial

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      // implement node event listeners here
+    },
+  },
+});
