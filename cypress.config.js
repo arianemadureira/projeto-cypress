7f477be7d688f1eb32766f2eedfaac5f30c7dae0
arianemadureira <arianemadureira@live.com>
2022-11-10 15:15:54 -0300

    Add mocha

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'qnk5xy',
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
